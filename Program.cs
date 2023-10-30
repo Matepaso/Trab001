6db3bcf68a3d1b9dc6757d7a5bb0d97314296322
Matepaso <pagteresa10@gmail.com>
2023-10-30 19:44:01 -0300

    Correção casa decimais

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using Trab001.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var defaultCulture = "pt-BR";
+var ci = new CultureInfo(defaultCulture);
+ci.NumberFormat.NumberDecimalSeparator = ".";
+ci.NumberFormat.CurrencyDecimalSeparator = ".";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,6 +14,19 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer("Data Source = DESKTOP-126PQJ2\\MSSQLMATEPASO; Initial Catalog = bd_loja; User = sa; Password = p@gnossim1542004; TrustServerCertificate = True;"));
 
 var app = builder.Build();
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(ci),
+    SupportedCultures = new List<CultureInfo>
+    {
+        ci,
+    },
+    SupportedUICultures = new List<CultureInfo>
+    {
+        ci,
+    }
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
